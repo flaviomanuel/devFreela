6393c50484880d9f5f6525cfcab418ce902c7942
Flávio Manuel <flaviomanuel258@gmail.com>
2022-12-29 23:36:02 -0300

    feat: implementa CQRS para os metodos Queries SkillsServices, ProjectsService

---
+++ DevFreela.API/Controllers/SkillController.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Application.Queries.GetAllSkills;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    [ApiController]
+    public class SkillController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SkillController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetAllSkillsQuery();
+
+            var skills = await _mediator.Send(query);
+
+            return Ok(skills);
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using DevFreela.Application.ViewModels;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+    public class GetAllSkillsQueryHandler : IRequestHandler<GetAllSkillsQuery, List<SkillViewModel>>
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
+        public GetAllSkillsQueryHandler(DevFreelaDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+
+        }
+        public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                var script = "SELECT Id, Description FROM Skills";
+
+                var skills = await sqlConnection.QueryAsync<SkillViewModel>(script);
+
+                return skills.ToList();
+            }
+        }
+    }
+}
