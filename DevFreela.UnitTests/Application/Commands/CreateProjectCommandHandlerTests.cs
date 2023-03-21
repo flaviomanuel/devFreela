dea83914af08e32005af0065922ec58d8c8a2f20
Flávio Manuel <flaviomanuel258@gmail.com>
2023-03-20 22:42:10 -0300

    test: create CreateProjectCommandHandlerTests

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            //Arrange
+        
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+
+            var createProjectCommand =  new CreateProjectCommand(){
+                Title="Title teste", 
+                Description= "Nice description",
+                TotalCost = 50000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepositoryMock.Object);
+
+            //Act
+
+            var id  = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+            //Assert 
+            Assert.True(id >= 0);
+            
+            projectRepositoryMock.Verify(x => x.CreateProjectAsync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
