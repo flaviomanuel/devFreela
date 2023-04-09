24fd88a53625c4003138874bd753528aa307c1ba
Flávio Manuel <flaviomanuel258@gmail.com>
2023-04-09 18:42:43 -0300

    feat(SkillRepository): create  method AddSkillFromProject()

--- DevFreela.Core/Repositories/ISkillRepository.cs
+++ DevFreela.Core/Repositories/ISkillRepository.cs
@@ -1,9 +1,11 @@
 ﻿using DevFreela.Core.DTOs;
+using DevFreela.Core.Entities;
 
 namespace DevFreela.Core.Repositories
 {
     public interface ISkillRepository
     {
         Task<List<SkillDTO>> GetSkillAsync();
+        Task AddSkillFromProject(Project project);
     }
 }
--- DevFreela.Infrastructure/Persistence/Repositories/SkillRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/SkillRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using DevFreela.Core.DTOs;
+using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
@@ -9,12 +10,22 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
     public class SkillRepository : ISkillRepository
     {
         private readonly string _connectionString;
-        public SkillRepository(IConfiguration configuration)
+        private readonly DevFreelaDbContext _context;
+        public SkillRepository(IConfiguration configuration, DevFreelaDbContext context)
         {
             _connectionString = configuration.GetConnectionString("DevFreelaCs");
+            _context = context;
         }
 
+        public async Task AddSkillFromProject(Project project)
+        {
+            var words = project.Description.Split(' ');
+            var length = words.Length;
 
+            var skill= $"{project.Id} - {words[length - 1]}";
+
+            await _context.Skills.AddAsync(new Skill(skill));
+        }
 
         public async Task<List<SkillDTO>> GetSkillAsync()
         {
